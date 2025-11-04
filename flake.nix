c120233e06f127505b9437d9487712b95ca1422d
João Thallis <joaothallis@icloud.com>
2025-11-04 17:20:34 +0000

    Add initial config

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ nixpkgs, nixos-wsl, home-manager, ... }: {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+	modules = [
+	  nixos-wsl.nixosModules.default
+	  home-manager.nixosModules.home-manager
+	  {
+	    system.stateVersion = "25.05";
+	    wsl.enable = true;
+
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.nixos = ./home.nix;
+	  }
+	];
+      };
+    };
+  };
+}
