6807c94b735612fbfcf0252f455729b970590fe9
João Thallis <joaothallis.developer@gmail.com>
2025-11-16 21:07:17 +0000

    Enable bash

--- home.nix
+++ home.nix
@@ -40,6 +40,7 @@
       enableBashIntegration = true;
       nix-direnv.enable = true;
     };
+        bash.enable = true; 
     tmux = {enable= true; escapeTime= 10; focusEvents=true; terminal="screen-256color";
           extraConfig = ''
           set-option -a terminal-features 'xterm-256color:RGB'
