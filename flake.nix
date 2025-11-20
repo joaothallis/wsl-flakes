315a8a7ffe60665e60b074ed3eed4e2c13f99f9c
João Thallis <joaothallis.developer@gmail.com>
2025-11-20 19:44:57 +0000

    Install gemini-cli

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-ai-tools.url = "github:numtide/nix-ai-tools";
   };
 
   outputs =
@@ -13,6 +14,7 @@
       nixpkgs,
       nixos-wsl,
       home-manager,
+      nix-ai-tools,
       ...
     }:
     {
@@ -22,7 +24,7 @@
           modules = [
             nixos-wsl.nixosModules.default
             home-manager.nixosModules.home-manager
-            {
+            ({ pkgs, ... }: {
               system.stateVersion = "25.05";
               wsl.enable = true;
 
@@ -30,10 +32,12 @@
                 EDITOR = "vim";
               };
 
+	      environment.systemPackages = with inputs.nix-ai-tools.packages.${pkgs.system}; [gemini-cli];
+
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.nixos = ./home.nix;
-            }
+            })
           ];
         };
       };
